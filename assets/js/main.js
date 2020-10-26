5a261ef179388cb8dfa37cf8ab132299c14f7740
nic-was <44951135+nic-was@users.noreply.github.com>
2020-10-26 10:51:50 +0000

    Adding HTML elements to the clients page to display all clients that have been added to localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ function addNewClient(new_client) {
 
     let clients = JSON.parse(localStorage.getItem("Clients")) || [];
             
-    clients.forEach(function(client){
+    clients.forEach(function(client) {
         if  (new_client.name ==  client.name) {
             client_exists += 1
             // break
@@ -19,10 +19,46 @@ function addNewClient(new_client) {
     if (client_exists == 0) {
         clients.push(new_client);
         localStorage.setItem("Clients",JSON.stringify(clients));
+        location.reload()
     } else {
         console.log(new_client.name + " already exists, please choose a different client name")
     }
 
 }      
+
+
+
+//adding clients
+function insertClientListAsHTML(parentElement) {
+    let clients = JSON.parse(localStorage.getItem("Clients")) || [];
+
+    if (clients.length === 0) {
+        return
+    }
+    parentElement.innerHTML = ""
+
+    let groups = _.groupBy(clients, 'group')
+    Object.keys(groups).forEach(group => {
+
+        let element = document.createElement('template')
+        let html = `<div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col">
+                                <h2>${group}</h2>`
+
+        Array.from(groups[group]).forEach(client => {
+
+            html += `<div class="d-flex flex-row justify-content-between" style="background : ${client.colour}">
+                    <div class="p-2"><h4>${client.name}</h4> 
+                    €${client.ratingph} p/h</div>
+                    </div>`
+                })
+            html += `</div></div></div>`
+        
+        element.innerHTML = html.trim()
+        element = element.content.firstChild
+        parentElement.appendChild(element)
+    })
+}
 
 
