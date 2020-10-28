b28488eb1ce9ba34c6426da5ae1cf3721ab849ab
nic-was <44951135+nic-was@users.noreply.github.com>
2020-10-28 23:15:04 +0000

    Adding timer function which starts and stops counting time when client name or time is clicked. Updates localstorage and the display every second

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,33 @@
+window.intervalId = null
+window.activeTimer = null
+
 //CLIENTS PAGE
 
-//1. addNewClient Function: This function should be called whenever the user clicks the SAVE NEW CLIENT button on the new clients form
-function addNewClient(new_client) {
+//1. addNewClient Function: This function should be called whenever the user clicks the button to add a new client
+function addNewClient(newClient) {
     //checking to see if a client exists already or not
-    let client_exists = 0;
+    let clientExists = 0;
 
     let clients = JSON.parse(localStorage.getItem("Clients")) || [];
             
     clients.forEach(function(client) {
-        if  (new_client.name.toLowerCase() ===  client.name.toLowerCase()) {
-            client_exists += 1
-            // break
+        if  (newClient.name.toLowerCase() ===  client.name.toLowerCase()) {
+            clientExists += 1
         } 
     });
 
     //if a client does already exist, an error message should pop up saying that the client already exists and cannot be added
     //user will need to add a different client
 
-    if (client_exists == 0) {
-        clients.push(new_client);
-        localStorage.setItem("Clients",JSON.stringify(clients));
+    if (clientExists === 0) {
+        clients.push(newClient);
+        localStorage.setItem("Clients", JSON.stringify(clients));
         location.reload()
     } else {
-        console.log(new_client.name + " already exists, please choose a different client name")
+        console.log(newClient.name + " already exists, please choose a different client name")
     }
 
 }      
-
-
 
 //2. addNewClientEventListener Function: Creating an event listener for the new client form submit button
 //   This will take inputs from the form and check them in the addNewClient function to make sure the client doesn't already exist
@@ -41,22 +41,21 @@ function addNewClientEventListener() {
     form.addEventListener('submit', (event) => {
         event.preventDefault()
         let data = new FormData(form)
-        let new_client = {  "name": data.get('name'),
+        let newClient = {  "name": data.get('name'),
                             "ratingph": data.get('ratingph'),
                             "colour": data.get('colour'),
                             "group": data.get('group').toLowerCase()
                         }
     
-        addNewClient(new_client)         
+        addNewClient(newClient)         
 
-        console.log("The submit button has been clicked!")
         form.reset()
         
     })
 }
 
 
-//3. adding clients to HTML page
+//3. adding client Divs to HTML page
 function insertClientListAsHTML(parentElement) {
     let clients = JSON.parse(localStorage.getItem("Clients")) || [];
 
@@ -74,58 +73,53 @@ function insertClientListAsHTML(parentElement) {
                                 <h2 class="text-capitalize">${group}</h2>
                                 <div class="d-flex flex-row flex-wrap">
                                 `
-
         Array.from(groups[group]).forEach(client => {
 
             html += `
-                    <div class="p-2 ml-2 col-sm-4 col-md-2 mb-2"  style="background : ${client.colour}">
+                    <div class="client_card p-2 ml-2 col-sm-4 col-md-2 mb-2"  style="color : ${client.colour}">
 
-                        <h4>${client.name}</h4> 
-                        <div>€${client.ratingph} p/h</div>
+                        <div class="client_name">${client.name}</div> 
+                        <div class="client_rating">€${client.ratingph} p/h</div>
                     </div>
                     `
                 })
             html += ` </div>
                     </div>
                 `
-        
+
         element.innerHTML = html.trim()
         element = element.content.firstChild
         parentElement.appendChild(element)
     })
 }
 
-
 //HOME PAGE (INDEX)
 
-
 //1. addNewClientToDay Function: This function should be called whenever the user clicks the SAVE NEW CLIENT button on the new clients to day form
-function addClientToDay(current_day, new_client_to_day) {
+function addClientToDay(currentDay, newClientToDay) {
      //checking to see if a client exists already or not
-    let client_exists = 0;
+    let clientExists = 0;
 
-    let clientsPerDay =  JSON.parse(window.localStorage.getItem(current_day)) || []
+    let clientsPerDay =  JSON.parse(window.localStorage.getItem(currentDay)) || []
             
     clientsPerDay.forEach(function(client) {
-        if  (new_client_to_day.name ===  client.name) {
-            client_exists += 1
-            // break
+        if  (newClientToDay.name ===  client.name) {
+            clientExists += 1
         } 
     });
 
     //if a client does already exist, an error message should pop up saying that the client already exists and cannot be added
     //user will need to add a different client
 
-    if (client_exists == 0) {
-        clientsPerDay.push(new_client_to_day);
-        localStorage.setItem(current_day, JSON.stringify(clientsPerDay));
+    if (clientExists == 0) {
+        clientsPerDay.push(newClientToDay);
+        localStorage.setItem(currentDay, JSON.stringify(clientsPerDay));
         location.reload()
     } else {
-        console.log(new_client_to_day.name + " already exists, please choose a different client name")
+        console.log(newClientToDay.name + " already exists, please choose a different client name")
     }
 
 }
-
 
 //2. addClientEventListener Function: Creating an event listener for the new client to day form submit button
 //   This will take inputs from the form and check them in the addClientToDay function to make sure the client doesn't already exist
@@ -133,12 +127,11 @@ function addClientToDay(current_day, new_client_to_day) {
 function addClientEventListener() {
     let form = document.getElementById('new-client-to-day');
            
-
     form.addEventListener('submit', (event) => {
         event.preventDefault()
         let data = new FormData(form)
-        let day_lookup = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        let current_day = day_lookup[new Date().getDay()]
+        let dayLookup = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let currentDay = dayLookup[new Date().getDay()]
         let clients = JSON.parse(localStorage.getItem("Clients")) || [];
   
         let client = clients.filter(client => client.name === data.get('name'))
@@ -147,20 +140,20 @@ function addClientEventListener() {
           return
         } 
   
-        let new_client_to_day = { "name": data.get('name'),
-                                  "group": client[0].group,
-                                  "seconds": 0 
+        let newClientToDay = {  "name": data.get('name'),
+                                "group": client[0].group,
+                                "seconds": 0 
                                 }
       
-        addClientToDay(current_day, new_client_to_day)         
+        addClientToDay(currentDay, newClientToDay)         
   
         form.reset()
     })}
   
 
-//3. adding clients from localstorage to Index.HTML page by day
+//3. insertClientToDayAsHTML Function: adding clients from localstorage to Index.HTML page by day
 
-// function to display the second recorded as time in HH:MM:SS format
+//3.1 displaySecondsAsTime Function: displays the second recorded as time in HH:MM:SS format
 function displaySecondsAsTime(seconds) {
     //https://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript
     return new Date(seconds * 1000).toISOString().substr(11, 8)
@@ -168,43 +161,84 @@ function displaySecondsAsTime(seconds) {
 
 function insertClientToDayAsHTML() {
 
-    let day_lookup = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    day_lookup.forEach(day => {
+    let dayLookup = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    dayLookup.forEach(day => {
        
+        let dayContainer = document.getElementById(day)
+        let clientsPerDay = JSON.parse(localStorage.getItem(day)) || [];
 
-        let clients_per_day = JSON.parse(localStorage.getItem(day)) || [];
+        let groups = _.groupBy(clientsPerDay, 'group')
 
-        let groups = _.groupBy(clients_per_day, 'group')
+        
+        if (clientsPerDay.length > 0) {
+            dayContainer.innerHTML = `<div class= " day_card">${day}</div>`
+        } else {
+            dayContainer.style.display = "none"
+        }
+
         Object.keys(groups).forEach(group => {
             let group_element = document.createElement('div')
+            group_element.innerHTML = `<h4 class="group_card">${group}  </h4><hr>`
             
             Array.from(groups[group]).forEach(client => {
                 let client_element = document.createElement('div')
                 client_element.className = 'client'
-                client_element.onclick = function(){
-                // TO UPDATE: starting the timer for the client that is clicked will happen in here                    
-                    alert(client.name)
-
-                    }
-                    client_element.innerHTML = `${client.name} ${displaySecondsAsTime(client.seconds)}`
-                    group_element.appendChild(client_element)
+                client_element.onclick = (target) => {
+                    activateClientTimer(client_element, client, day)
+                }
+                client_element.innerHTML = `
+                    <div class="client-time-card d-flex flex-column p-2 ">
+                        <div class="client-name">${client.name}</div>
+                        <div class="timer">${displaySecondsAsTime(client.seconds)}</div>
+                    </div>
+                `
+                group_element.appendChild(client_element)
             
             })
-            document.getElementById(day).appendChild(group_element)
+            dayContainer.appendChild(group_element)
         })
-        })
+    })
 }
 
+// 4.   activateClientTimer Function: This function will activate the timer when clicking on a client
+//      this is only possible for the current day!
+function activateClientTimer(clientElement, client, day) {
+    
+    let clientsPerDay = JSON.parse(localStorage.getItem(day)) || [];
+    let c = clientsPerDay.filter(x => x.name === client.name)[0]
+    let timerElement = clientElement.querySelector(".timer")
 
-//4. Get current day as string
+    if (day !== getCurrentDay()) {
+        return
+    }
+    document.querySelectorAll(".active-timer").forEach(x => x.classList.remove("active-timer"))
+
+    // Disable current timer
+    if (window.intervalId) {
+        clearInterval(window.intervalId)
+    }
+    
+    if (window.activeTimer === clientElement) {
+        window.activeTimer = null
+        return
+    }
+
+    clientElement.classList.add("active-timer")
+    window.activeTimer = clientElement
+    window.intervalId = setInterval(() => {
+        c.seconds++
+        timerElement.innerHTML = displaySecondsAsTime(c.seconds)
+        localStorage.setItem(day, JSON.stringify(clientsPerDay));
+    }, 1000);
+}
+
+// 5. Get current day as string
 function getCurrentDay() {
-    let day_lookup = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    return day_lookup[new Date().getDay()]
+    let dayLookup = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return dayLookup[new Date().getDay()]
 }
 
-
-
-//5. Populating options list for clients that have not already been selected for today's date 
+// 6. insertAvailableClientListAsHTML Function: Populates options list for clients that have not already been selected for today's date 
 function insertAvailableClientListAsHTML(parentElement) {
     let clients = JSON.parse(localStorage.getItem("Clients")) || [];
     let clientsInDay = (JSON.parse(localStorage.getItem(getCurrentDay())) || []).map(x => x.name)
@@ -221,3 +255,5 @@ function insertAvailableClientListAsHTML(parentElement) {
     })
     
 }
+
+
